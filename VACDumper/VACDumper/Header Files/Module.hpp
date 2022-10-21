ab6ea89a90ff1073cc740ff72a47508bd90d3d11
Joseph Kerr <114572797+altoid29@users.noreply.github.com>
2022-10-21 16:39:24 +0100

    Update Module.hpp

--- VACDumper/VACDumper/Header Files/Module.hpp
+++ VACDumper/VACDumper/Header Files/Module.hpp
@@ -7,20 +7,20 @@
 class Module
 {
 public:
-	std::uint32_t  unCRC32;
-	HMODULE		   _module;
-	void*		   modulePointer;
+	std::uint32_t unCRC32;
+	HMODULE _module;
+	void* modulePointer;
 	int(__stdcall* EntryPointFn)(std::uint32_t functionId, void const* parameters, std::uint32_t parametersSize, void* result, std::uint32_t* resultSize);
-	std::int32_t   lastResult;
+	std::int32_t lastResult;
 
 	// Size of the region for the module in memory.
-	std::uint32_t  moduleSize;
+	std::uint32_t moduleSize;
 
 	// Actual memory region address.
-	void*		   rawModule;
+	void* rawModule;
 
-	std::uint32_t  unknown1C;
-	std::uint32_t  unknown20;
+	std::uint32_t unknown1C;
+	std::uint32_t unknown20;
 };
 
 // -------------------------------------------------------------------------------------------------------------------
